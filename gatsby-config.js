346a7daa16839598afd5a3b9ed891823081d28a7
abdelamhorizont <abdelhamid.ameur@hotmail.de>
2022-07-27 11:11:09 +0200

    datoCms template

---
+++ gatsby-config.js
@@ -0,0 +1,40 @@
+// support for .env, .env.development, and .env.production
+require("dotenv").config()
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://gatsbydatocmshomepage.gatsbyjs.io/",
+    title: "SUPERSUPPLY",
+    author: `abdel am meer`,
+    description: "",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-datocms",
+      options: {
+        apiToken: process.env.DATOCMS_API_TOKEN,
+        environment: process.env.DATOCMS_ENVIRONMENT,
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-image",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-vanilla-extract",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "SUPERSUPPLY",
+        short_name: "SUPERSUPPLY",
+        start_url: "/",
+        // These can be imported once ESM support lands
+        background_color: "#ffffff",
+        theme_color: "#db3000",
+        icon: "src/favicon.png",
+      },
+    },
+  ],
+}
